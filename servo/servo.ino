fc79bae320c0bced041921f8321abc5b329782a5
chazyu1996 <chazyu1996@gmail.com>
2020-10-30 22:27:40 +0800

    1

--- servo/servo.ino
+++ servo/servo.ino
@@ -1,28 +1,29 @@
-int apin=8;
-int bpin=9;
-int cpin=10;
-int dpin=11;
-int delaytime=5;
+int apin = 8;
+int bpin = 9;
+int cpin = 10;
+int dpin = 11;
+int delaytime = 5;
 
-void setup() {
-pinMode(apin,OUTPUT);
-pinMode(bpin,OUTPUT);
-pinMode(cpin,OUTPUT);
-pinMode(dpin,OUTPUT);
+void setup()
+{
+    pinMode(apin, OUTPUT);
+    pinMode(bpin, OUTPUT);
+    pinMode(cpin, OUTPUT);
+    pinMode(dpin, OUTPUT);
 }
 
-void loop() {
- digitalWrite(dpin,LOW);
-digitalWrite(apin,HIGH);
-delay(delaytime);
-digitalWrite(apin,LOW);
-digitalWrite(bpin,HIGH);
-delay(delaytime);
-digitalWrite(bpin,LOW);
-digitalWrite(cpin,HIGH);
-delay(delaytime);
-digitalWrite(cpin,LOW);
-digitalWrite(dpin,HIGH);
-delay(delaytime);
-
-} 
+void loop()
+{
+    digitalWrite(dpin, LOW);
+    digitalWrite(apin, HIGH);
+    delay(delaytime);
+    digitalWrite(apin, LOW);
+    digitalWrite(bpin, HIGH);
+    delay(delaytime);
+    digitalWrite(bpin, LOW);
+    digitalWrite(cpin, HIGH);
+    delay(delaytime);
+    digitalWrite(cpin, LOW);
+    digitalWrite(dpin, HIGH);
+    delay(delaytime);
+}
